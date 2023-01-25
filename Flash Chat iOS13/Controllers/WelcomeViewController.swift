0d29d855039e065c38e8f0750902e70d3d4849f1
Djordje Knezevic <starwarrior@mycity.rs>
2023-01-25 19:30:37 +0100

    Additionally style navigation bar

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -17,16 +17,6 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         
         titleLabel.text = Constants.appName
-
-//        titleLabel.text = ""
-//        var charIndex = 0.0
-//        let titleText = "⚡️FlashChat"
-//        for letter in titleText {
-//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-//                self.titleLabel.text?.append(letter)
-//            }
-//            charIndex += 1
-//        }
     
     }
     
